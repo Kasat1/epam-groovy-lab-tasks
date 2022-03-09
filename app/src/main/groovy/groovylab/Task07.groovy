f96658987a82233a2761bee3cf6bedd6f46b1206
Kasat1 <99502652+Kasat1@users.noreply.github.com>
2022-03-09 09:14:38 +0300

    Update Task07.groovy

--- app/src/main/groovy/groovylab/Task07.groovy
+++ app/src/main/groovy/groovylab/Task07.groovy
@@ -12,25 +12,7 @@ Task07.encryptThis("hello world") == "104olle 119drlo"
 package groovylab
 
 class Task07 {
-    public static String encryptThis(String s) {
-        if(s.length()==0) return"";
-        List words = s.split(" ") 
-        def res = []
-        for (def i in words){
-          if (i.length() <= 2){
-            if(i.length() <= 1){res << (int) i[0]}
-            else {res << (int) i[0]+i[1]}
-          }
-          else{
-            res << (int) i[0] + i[-1] + i[2..-2] + i[1]
-          }
-        }
-        return res.join(" ")
-          // for (int i=0;i<words.count;i++){
-          //     return  (int) words[i][0]+words[i][-1]+words[i][2,3,1]}
-        //return words[0][0]
-    }  
-    static void main(String[] args) {
-      println Task07.encryptThis("Hi Peter PArker")
-}
+  public static String encryptThis(String s) {
+    return s.split(' ').collect{(int) it[0] + it[-1..1]}.join(' ')
+  }
 }
